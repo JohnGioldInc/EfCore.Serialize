ac8fdf0d578867783b6fd0511cfe23b3d4aab70f
John Gold Inc <John.T.Gold@gmail.com>
2025-03-22 14:17:09 -0400

    Serialized , Deserializes, Issue with Casting after Deserialize

--- src/EfCore.Serialize/Extensions/SerializeServiceCollectionExtensions.cs
+++ src/EfCore.Serialize/Extensions/SerializeServiceCollectionExtensions.cs
@@ -3,7 +3,9 @@
 
 using System.ComponentModel;
 using System.Linq.Expressions;
+using JohnGoldInc.EntityFrameworkCore.Serialize.Query.Internal;
 using JohnGoldInc.EntityFrameworkCore.Serialize.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -38,7 +40,9 @@ public static class SerializeServiceCollectionExtensions
     {
         serviceCollection.AddEntityFrameworkInMemoryDatabase();
         serviceCollection.RemoveAll<IDatabase>();
-        serviceCollection.TryAddScoped<IDatabase>((sp) => new SerializeDatabase(dataProvider, saveChangesAsync, serializer));
+        serviceCollection.TryAddScoped<IDatabase>((sp) => new SerializeDatabase(saveChangesAsync));
+        serviceCollection.RemoveAll<IAsyncQueryProvider>();
+        serviceCollection.TryAddScoped<IAsyncQueryProvider>((sp) => new SerializeQueryProvider(dataProvider, serializer));
 
         return serviceCollection;
     }
--- src/EfCore.Serialize/Storage/Internal/SerializeDatabase.cs
+++ src/EfCore.Serialize/Storage/Internal/SerializeDatabase.cs
@@ -14,82 +14,21 @@ namespace JohnGoldInc.EntityFrameworkCore.Serialize.Storage.Internal
     /// </summary>
     public class SerializeDatabase : IDatabase
     {
-        private readonly Func<string, CancellationToken, Task<string>> dataProvider;
         private readonly Func<IEnumerable<IUpdateEntry>, CancellationToken, Task<int>> saveChangesAsync;
-        private readonly Func<Expression, string> serializer;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SerializeDatabase"/> class.
         /// </summary>
-        public SerializeDatabase(Func<string, CancellationToken, Task<string>> dataProvider, Func<IEnumerable<IUpdateEntry>, CancellationToken, Task<int>> saveChangesAsync, Func<Expression,string> serializer)
-        {
-            this.dataProvider = dataProvider;
-            this.saveChangesAsync = saveChangesAsync;
-            this.serializer = serializer;
-        }
+        public SerializeDatabase(Func<IEnumerable<IUpdateEntry>, CancellationToken, Task<int>> saveChangesAsync)
+            => this.saveChangesAsync = saveChangesAsync;
 
-        /// <summary>
-        /// Compiles the specified query.
-        /// </summary>
-        /// <typeparam name="TResult">The type of the result.</typeparam>
-        /// <param name="query">The query expression.</param>
-        /// <param name="async">if set to <see langword="true"/> [asynchronous].</param>
-        /// <returns>A function that executes the query.</returns>
+        /// <inheritdoc />
         public Func<QueryContext, TResult> CompileQuery<TResult>(Expression query, bool async)
-            => CompileQueryExpression<TResult>(query, async).Compile();
+             => throw new NotImplementedException();
 
-        /// <summary>
-        /// Compiles the query expression.
-        /// </summary>
-        /// <typeparam name="TResult">The type of the result.</typeparam>
-        /// <param name="query">The query expression.</param>
-        /// <param name="async">if set to <see langword="true"/> [asynchronous].</param>
-        /// <param name="nonNullableReferenceTypeParameters">The non-nullable reference type parameters.</param>
-        /// <returns>An expression that represents the compiled query.</returns>
+        /// <inheritdoc />
         public Expression<Func<QueryContext, TResult>> CompileQueryExpression<TResult>(Expression query, bool async, IReadOnlySet<string>? nonNullableReferenceTypeParameters = default)
-        {
-            query = ReplaceEntityQueryRootExpressionVisitor.Instance.Visit(query);
-
-            var serialized = serializer(query);
-            var genericType = typeof(TResult).GetGenericArguments()?.FirstOrDefault()?.GetGenericArguments()?.FirstOrDefault()
-                ?? typeof(TResult).GetGenericArguments()?.FirstOrDefault()
-                ?? typeof(TResult);
-            var queryingEnumerableType = typeof(QueryingEnumerable<>).MakeGenericType(genericType);
-
-            var data = dataProvider(serialized, CancellationToken.None);
-
-            var queryContextParameter = Expression.Parameter(typeof(QueryContext), "queryContext");
-
-            var dataParameter = Expression.Constant(data);
-
-            var newQueryingEnumerable = Expression.New(queryingEnumerableType.GetConstructor(new[] { typeof(Task<string>) })!, dataParameter);
-
-            var lambda = Expression.Lambda<Func<QueryContext, TResult>>(newQueryingEnumerable, queryContextParameter);
-            return lambda;
-        }
-
-        private class ReplaceEntityQueryRootExpressionVisitor : ExpressionVisitor
-        {
-            public static readonly ReplaceEntityQueryRootExpressionVisitor Instance = _Instance ??= new ReplaceEntityQueryRootExpressionVisitor();
-            private static ReplaceEntityQueryRootExpressionVisitor _Instance;
-
-            /// <summary>
-            ///     Rewrites <see cref="EntityQueryRootExpression" /> encountered in an expression to use a different entity type.
-            /// </summary>
-            /// <param name="expression">The query expression to rewrite.</param>
-            public Expression Rewrite(Expression expression)
-                => Visit(expression);
-
-            /// <inheritdoc />
-            protected override Expression VisitExtension(Expression extensionExpression)
-                => extensionExpression is EntityQueryRootExpression entityQueryRootExpression
-                    ? Expression.Parameter(typeof(IQueryable<>).MakeGenericType(entityQueryRootExpression.EntityType.ClrType), "queryable")
-                    : base.VisitExtension(extensionExpression);
-
-            /// <inheritdoc />
-            protected override Expression VisitParameter(ParameterExpression parameterExpression)
-                => base.VisitParameter(parameterExpression);
-        }
+            => throw new NotImplementedException();
 
         /// <summary>
         /// Saves the changes.
@@ -108,64 +47,5 @@ namespace JohnGoldInc.EntityFrameworkCore.Serialize.Storage.Internal
         public Task<int> SaveChangesAsync(IList<IUpdateEntry> entries, CancellationToken cancellationToken = default)
             => this.saveChangesAsync(entries, cancellationToken);
 
-        private sealed class QueryingEnumerable<T>(Task<string> data)
-            : IAsyncEnumerable<T>, IEnumerable<T>
-             where T : class
-        {
-            private QueryingEnumerator<T>? enumerator;
-
-            public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
-                => enumerator ?? (enumerator = new QueryingEnumerator<T>(data, cancellationToken));
-
-            public IEnumerator<T> GetEnumerator()
-                 => enumerator ?? (enumerator = new QueryingEnumerator<T>(data));
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-            private sealed class QueryingEnumerator<E>(Task<string> data, CancellationToken cancellationToken = default) : IAsyncEnumerator<E>, IEnumerator<E>
-                where E : class
-            {
-                private IEnumerable<E>? enumerable;
-                private IEnumerator<E>? enumerableEnumerator;
-
-                public E Current=> (E)enumerableEnumerator!.Current!;
-
-                E IEnumerator<E>.Current => Current;
-
-                object IEnumerator.Current => Current;
-
-                public void Dispose()
-                {
-                    enumerableEnumerator?.Dispose();
-                    enumerableEnumerator = null;
-                    enumerable = null;
-                }
-
-                public ValueTask DisposeAsync()
-                {
-                    this.Dispose();
-                    return ValueTask.CompletedTask;
-                }
-
-                public bool MoveNext()
-                    => MoveNextAsync().AsTask().Result;
-
-                public async ValueTask<bool> MoveNextAsync()
-                {
-                    if (cancellationToken.IsCancellationRequested)
-                    {
-                        await DisposeAsync().ConfigureAwait(false);
-                        cancellationToken.ThrowIfCancellationRequested();
-                    }
-
-                    enumerable ??= Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<E>>(await data.ConfigureAwait(false));
-                    enumerableEnumerator ??= enumerable!.GetEnumerator();
-
-                    return enumerableEnumerator!.MoveNext();
-                }
-
-                public void Reset() => throw new NotImplementedException();
-            }
-        }
     }
 }
